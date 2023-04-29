3eeae566ad46760f0ee5e4b8b2c0339e551f0cd0
Bruno Grande <bruno.grande@sagebase.org>
2023-04-29 00:17:16 -0700

    Address PR comments

--- optimize-nextflow.py
+++ optimize-nextflow.py
@@ -28,8 +28,8 @@ def from_json(metrics_path: Path):
         tw --output "json" runs view -w "<org>/<workspace>" -i "<workflow-id>" metrics
     """
     json_parser = MetricsJsonParser(metrics_path)
-    task_metrics = json_parser.parse()
-    config_generator = NextflowConfigGenerator(task_metrics)
+    process_metrics = json_parser.parse()
+    config_generator = NextflowConfigGenerator(process_metrics)
     config = config_generator.generate()
     print(config)
 
@@ -40,11 +40,13 @@ def from_api():
 
     Not implemented yet.
     """
-    ...
+    raise NotImplementedError()
 
 
 @dataclass
 class Stats(Generic[Number]):
+    """Suite of statistics."""
+
     q1: Number
     q2: Number
     q3: Number
@@ -54,11 +56,14 @@ class Stats(Generic[Number]):
 
     @cached_property
     def max_q3_ratio(self):
+        """Ratio between the max and Q3 values."""
         return round(self.max / self.q3, 2)
 
 
 @dataclass
-class TaskMetrics:
+class ProcessMetrics:
+    """Set of metrics for a process."""
+
     name: str
     mem: Stats
     mem_pct: Stats
@@ -67,27 +72,58 @@ class TaskMetrics:
 
     @staticmethod
     def bytes_to_gb(memory: int) -> int:
+        """Convert memory in bytes to GB (with some buffer).
+
+        Args:
+            memory: Memory in bytes.
+
+        Returns:
+            Memory in GB (with some buffer).
+        """
         memory_gb = memory / 1024**3
         return ceil(memory_gb + 0.5)
 
     @staticmethod
     def load_to_cores(load: float) -> int:
+        """Convert CPU load to number of cores (with some buffer).
+
+        Args:
+            load: CPU load in percentage.
+
+        Returns:
+            Number of CPU cores (with some buffer).
+        """
         cores = load / 100
         return ceil(cores + 0.5)
 
     @cached_property
     def mem_gb(self) -> Stats:
+        """Retrieve memory statistics in GB.
+
+        Returns:
+            Memory statistics in GB.
+        """
         values = astuple(self.mem)
         values_adj = [self.bytes_to_gb(x) for x in values]
         return Stats(*values_adj)
 
     @cached_property
     def cpu_cores(self) -> Stats:
+        """Retrieve CPU statistics in cores.
+
+        Returns:
+            CPU statistics in cores.
+        """
         values = astuple(self.cpu)
         values_adj = [self.load_to_cores(x) for x in values]
         return Stats(*values_adj)
 
     def optimize_mem(self) -> int:
+        """Optimize memory allocation based on heuristics.
+
+        Returns:
+            Optimized memory allocation.
+        """
         if self.mem.max == 1 and self.mem_pct.max <= 80:
             return self.mem_gb.max
         elif self.mem_gb.max <= 8:
@@ -100,6 +136,11 @@ class TaskMetrics:
             return self.mem_gb.q3
 
     def optimize_cpu(self) -> int:
+        """Optimize CPU allocation based on heuristics.
+
+        Returns:
+            Optimized CPU allocation.
+        """
         if self.cpu_cores.max_q3_ratio <= 2 and self.cpu_cores.max <= 8:
             return self.cpu_cores.max
         elif self.cpu_cores.max_q3_ratio <= 1.5 and self.cpu_cores.max <= 16:
@@ -110,62 +151,97 @@ class TaskMetrics:
 
 @dataclass
 class MetricsJsonParser:
+    """Parser for JSON file with Tower process metrics."""
+
     json_path: Path
 
     RawPctPair = namedtuple("RawPctPair", ["raw", "pct"])
 
     @cached_property
     def json(self):
+        """De-serialized JSON contents."""
         return json.load(self.json_path.open())
 
-    def parse(self) -> list[TaskMetrics]:
+    def parse(self) -> list[ProcessMetrics]:
+        """Parse JSON file with Tower process metrics.
+
+        Returns:
+            List of Tower process metrics.
+        """
         mem_metrics = self.parse_resource("metricsMemory", "memRaw", "memUsage")
         cpu_metrics = self.parse_resource("metricsCpu", "cpuRaw", "cpuUsage")
-        tasks = set(mem_metrics) & set(cpu_metrics)
+        processes = set(mem_metrics) & set(cpu_metrics)
 
-        all_task_metrics = list()
-        for task in tasks:
-            mem = mem_metrics[task]
-            cpu = cpu_metrics[task]
-            task_metrics = TaskMetrics(task, mem.raw, mem.pct, cpu.raw, cpu.pct)
-            all_task_metrics.append(task_metrics)
-        return all_task_metrics
+        all_process_metrics = list()
+        for process in processes:
+            mem = mem_metrics[process]
+            cpu = cpu_metrics[process]
+            process_metrics = ProcessMetrics(
+                process, mem.raw, mem.pct, cpu.raw, cpu.pct
+            )
+            all_process_metrics.append(process_metrics)
+        return all_process_metrics
 
     def parse_resource(self, top_key, raw_key, pct_key) -> dict[str, RawPctPair]:
+        """Parse the statistics for a specific resource.
+
+        Args:
+            top_key: Resource-specific top-level key.
+            raw_key: Key for raw resource usage.
+            pct_key: Key for percent resource usage.
+
+        Returns:
+            Resource usage statistics for each process.
+        """
         metrics = dict()
-        for task in self.json[top_key]:
-            task_name, task_metrics = task.popitem()
-            if not task_metrics:
+        for process in self.json[top_key]:
+            process_name, process_metrics = process.popitem()
+            if not process_metrics:
                 continue
-            usage_raw = Stats(**task_metrics[raw_key])
-            usage_pct = Stats(**task_metrics[pct_key])
-            metrics[task_name] = self.RawPctPair(usage_raw, usage_pct)
+            usage_raw = Stats(**process_metrics[raw_key])
+            usage_pct = Stats(**process_metrics[pct_key])
+            metrics[process_name] = self.RawPctPair(usage_raw, usage_pct)
         return metrics
 
 
 @dataclass
 class NextflowConfigGenerator:
-    task_metrics: list[TaskMetrics]
+    """Generator for Nextflow configuration."""
+
+    process_metrics: list[ProcessMetrics]
 
     def generate(self) -> str:
+        """Generate Nextflow configuration based on process metrics.
+
+        Returns:
+            Serialized Nextflow configuration.
+        """
         prefix = "process { \n"
         suffix = "\n }"
 
-        task_configs = list()
-        for task in self.task_metrics:
-            task_config = self.generate_single(task)
-            task_config_fmt = indent(task_config, "  ")
-            task_configs.append(task_config_fmt)
-        task_configs_concat = "".join(task_configs)
+        process_configs = list()
+        for process in self.process_metrics:
+            process_config = self.generate_single(process)
+            process_config_fmt = indent(process_config, "  ")
+            process_configs.append(process_config_fmt)
+        process_configs_concat = "".join(process_configs)
 
-        return prefix + task_configs_concat + suffix
+        return prefix + process_configs_concat + suffix
 
     @staticmethod
-    def generate_single(task_metrics: TaskMetrics) -> str:
-        mem_optimal = task_metrics.optimize_mem()
-        cpu_optimal = task_metrics.optimize_cpu()
+    def generate_single(process_metrics: ProcessMetrics) -> str:
+        """Generate Nextflow configuration for a single process.
+
+        Args:
+            process_metrics: Resource metrics for a single process.
+
+        Returns:
+            Serialized Nextflow configuration snippet.
+        """
+        mem_optimal = process_metrics.optimize_mem()
+        cpu_optimal = process_metrics.optimize_cpu()
         config = f"""
-            withName: { task_metrics.name } {{
+            withName: { process_metrics.name } {{
                 memory = { mem_optimal }.GB
                 cpu = { cpu_optimal }
             }}
